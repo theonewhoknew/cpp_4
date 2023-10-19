e96c70e76011c412c0c583de2f63fbf9e19db912
theonewhoknew <theonewhoknew@protonmail.com>
2023-10-19 10:00:04 +0200

    remove extra files

--- ex00/tmp/Dog.d
+++
@@ -1,5 +0,0 @@
-tmp/Dog.o: src/Dog.cpp src/../inc/Animal.hpp src/../inc/Dog.hpp \
- src/../inc/Animal.hpp
-src/../inc/Animal.hpp:
-src/../inc/Dog.hpp:
-src/../inc/Animal.hpp:
--- ex00/tmp/WrongAnimal.d
+++
@@ -1,2 +0,0 @@
-tmp/WrongAnimal.o: src/WrongAnimal.cpp src/../inc/WrongAnimal.hpp
-src/../inc/WrongAnimal.hpp:
