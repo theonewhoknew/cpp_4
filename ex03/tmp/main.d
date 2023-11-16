77df5af6ed04a5f3ec273fe0a755ebc0ce519a23
Diego Tome Perez <dtome-pe@car6s4.42barcelona.com>
2023-11-16 10:46:44 +0100

    update

--- ex03/tmp/main.d
+++
@@ -1,6 +0,0 @@
-tmp/main.o: src/main.cpp src/../inc/AMateria.hpp src/../inc/Ice.hpp \
- src/../inc/AMateria.hpp src/../inc/Cure.hpp
-src/../inc/AMateria.hpp:
-src/../inc/Ice.hpp:
-src/../inc/AMateria.hpp:
-src/../inc/Cure.hpp:
