462b51b8677f9cb39a801acf51504dffdfc44e08
Diego Tome Perez <dtome-pe@car6s4.42barcelona.com>
2023-11-16 10:49:43 +0100

    remove extra files

--- ex03/tmp/Cure.d
+++
@@ -1,8 +0,0 @@
-tmp/Cure.o: src/Cure.cpp src/../inc/Cure.hpp src/../inc/AMateria.hpp \
-  src/../inc/ICharacter.hpp
-
-src/../inc/Cure.hpp:
-
-src/../inc/AMateria.hpp:
-
-src/../inc/ICharacter.hpp:
--- ex03/tmp/Ice.d
+++
@@ -1,8 +0,0 @@
-tmp/Ice.o: src/Ice.cpp src/../inc/Ice.hpp src/../inc/AMateria.hpp \
-  src/../inc/ICharacter.hpp
-
-src/../inc/Ice.hpp:
-
-src/../inc/AMateria.hpp:
-
-src/../inc/ICharacter.hpp:
